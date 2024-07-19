27d37f08c1845710aaf7570a4dc183d6105b1319
snowdream <sn0wdr1am@qq.com>
2024-07-19 22:40:49 +0800

    feat: shell format

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ DOCKER_HUB_PROJECT=snowdreamtech/samba
 GITHUB_PROJECT=ghcr.io/snowdreamtech/samba
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
--t ${DOCKER_HUB_PROJECT}:latest \
--t ${DOCKER_HUB_PROJECT}:4.19.6 \
--t ${DOCKER_HUB_PROJECT}:4.19 \
--t ${DOCKER_HUB_PROJECT}:4.19 \
--t ${GITHUB_PROJECT}:latest \
--t ${GITHUB_PROJECT}:4.19.6 \
--t ${GITHUB_PROJECT}:4.19 \
--t ${GITHUB_PROJECT}:4.19 \
-. \
---push
+    -t ${DOCKER_HUB_PROJECT}:latest \
+    -t ${DOCKER_HUB_PROJECT}:4.19.6 \
+    -t ${DOCKER_HUB_PROJECT}:4.19 \
+    -t ${DOCKER_HUB_PROJECT}:4.19 \
+    -t ${GITHUB_PROJECT}:latest \
+    -t ${GITHUB_PROJECT}:4.19.6 \
+    -t ${GITHUB_PROJECT}:4.19 \
+    -t ${GITHUB_PROJECT}:4.19 \
+    . \
+    --push
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # change the samba password for root
-echo -e "$SAMBA_ROOT_PASSWORD\n$SAMBA_ROOT_PASSWORD" | smbpasswd -a -s root > /dev/null 2>&1
+echo -e "$SAMBA_ROOT_PASSWORD\n$SAMBA_ROOT_PASSWORD" | smbpasswd -a -s root >/dev/null 2>&1
 
 # start samba
 /usr/sbin/smbd -D -F -s /etc/samba/smb.conf
