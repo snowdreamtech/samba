d5a0742b343bf4426166de3d560d8f26a4456266
snowdream <sn0wdr1am@qq.com>
2025-02-04 15:53:11 -0500

    feat: Samba

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# change the samba password for user samba
+echo -e "${SAMBA_PASSWORD}\n${SAMBA_PASSWORD}" | smbpasswd >/dev/null 2>&1
+
+# start samba
+/usr/sbin/smbd -D -s /etc/samba/smb.conf
+
 # exec commands
 if [ -n "$*" ]; then
     sh -c "$*"
@@ -10,6 +16,7 @@ fi
 # https://github.com/docker/compose/issues/1926#issuecomment-422351028
 if [ "${KEEPALIVE}" -eq 1 ]; then
     trap : TERM INT
-    tail -f /dev/null & wait
+    tail -f /dev/null &
+    wait
     # sleep infinity & wait
 fi
